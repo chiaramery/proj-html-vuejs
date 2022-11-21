580667136a6f3268930591d93b94598d96f00813
chiaramery <meringolachiara@gmail.com>
2022-11-21 15:38:59 +0100

    Mappa interattiva

--- src/components/AppMap.vue
+++ src/components/AppMap.vue
@@ -6,7 +6,10 @@ export default {
 
 <template>
     <section id="map">
-        <img src="../assets/img/mappa.png" alt="">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2801.66143779677!2d-75.67874428437484!3d45.39599997910027!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xef374fb5a3427474!2zNDXCsDIzJzQ1LjYiTiA3NcKwNDAnMzUuNiJX!5e0!3m2!1sit!2sit!4v1669041431969!5m2!1sit!2sit"
+            width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
     </section>
 </template>
 
